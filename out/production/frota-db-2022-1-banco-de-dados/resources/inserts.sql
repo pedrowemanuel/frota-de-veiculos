fe7ee86b16911ba146706558f54e7c393d4aa642
pedrowemanuel <pedronuel36@gmail.com>
2022-07-02 15:33:49 -0300

    Initial commit

---
+++ out/production/frota-db-2022-1-banco-de-dados/resources/inserts.sql
@@ -0,0 +1,15 @@
+--MARCA
+INSERT INTO marca (descricao) VALUES ('Toyota');
+
+--MARCA PNEU
+INSERT INTO marca_pneu (descricao) VALUES ('goodyear');
+INSERT INTO marca_pneu (descricao) VALUES ('pirelli');
+INSERT INTO marca_pneu (descricao) VALUES ('michelin');
+INSERT INTO marca_pneu (descricao) VALUES ('toyo');
+INSERT INTO marca_pneu (descricao) VALUES ('dayton');
+INSERT INTO marca_pneu (descricao) VALUES ('multi-mile');
+INSERT INTO marca_pneu (descricao) VALUES ('kenda');
+INSERT INTO marca_pneu (descricao) VALUES ('firestone');
+INSERT INTO marca_pneu (descricao) VALUES ('gt radial');
+INSERT INTO marca_pneu (descricao) VALUES ('marshal');
+INSERT INTO marca_pneu (descricao) VALUES ('yokohama');
